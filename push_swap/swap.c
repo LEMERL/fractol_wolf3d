c023a2de9ec402fab479f825cdf3cc85de86f9e5
Merlin GRIMALDI <mgrimald@e2r12p2.42.fr>
2015-03-20 15:40:45 +0100

    MAJ

--- push_swap/swap.c
+++ push_swap/swap.c
@@ -6,7 +6,7 @@
 /*   By: mgrimald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/11 18:55:52 by mgrimald          #+#    #+#             */
-/*   Updated: 2015/03/11 20:25:22 by mgrimald         ###   ########.fr       */
+/*   Updated: 2015/03/17 14:02:59 by mgrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,8 @@ void	sw_swap_data(t_lst *a, t_lst *b)
 }
 
 
-void	reverse_rotate_a(t_lst **pile_a, t_opt opt)
+void	reverse_rotate(t_lst **pile_a)
 {
-	(void)opt;
 	t_lst	*tmp;
 	t_lst	*other;
 
@@ -41,22 +40,27 @@ void	reverse_rotate_a(t_lst **pile_a, t_opt opt)
 	}
 }
 
+void	reverse_rotate_a(t_lst **pile_a, t_opt opt)
+{
+	(void)opt;
+	reverse_rotate(pile_a);
+}
+
 void	reverse_rotate_b(t_lst **pile_b, t_opt opt)
 {
 	(void)opt;
-	reverse_rotate_a(pile_b, 0);
+	reverse_rotate(pile_b);
 }
 
 void	reverse_rotate_f(t_lst **pile_a, t_lst **pile_b, t_opt opt)
 {
 	(void)opt;
-	reverse_rotate_a(pile_a, 0);
-	reverse_rotate_b(pile_b, 0);
+	reverse_rotate(pile_a);
+	reverse_rotate(pile_b);
 }
 
-void	rotate_a(t_lst **pile_a, t_opt opt)
+void	rotate(t_lst **pile_a)
 {
-	(void)opt;
 	t_lst	*tmp;
 	t_lst	*other;
 
@@ -73,17 +77,23 @@ void	rotate_a(t_lst **pile_a, t_opt opt)
 	}
 }
 
+void	rotate_a(t_lst **pile_a, t_opt opt)
+{
+	(void)opt;
+	rotate(pile_a);
+}
+
 void	rotate_b(t_lst **pile_b, t_opt opt)
 {
 	(void)opt;
-	rotate_a(pile_b, 0);
+	rotate(pile_b);
 }
 
 void	rotate_f(t_lst **pile_a, t_lst **pile_b, t_opt opt)
 {
 	(void)opt;
-	rotate_a(pile_a, 0);
-	rotate_b(pile_b, 0);
+	rotate(pile_a);
+	rotate(pile_b);
 }
 
 void	swap_a(t_lst **pile_a, t_opt opt)
@@ -95,12 +105,12 @@ void	swap_a(t_lst **pile_a, t_opt opt)
 void	swap_b(t_lst **pile_b, t_opt opt)
 {
 	(void)opt;
-	swap_a(pile_b, 0);
+	sw_swap_data(getn_from_start(*pile_b, 0), getn_from_end(*pile_b, 0));
 }
 
 void	swap_f(t_lst **pile_a, t_lst **pile_b, t_opt opt)
 {
 	(void)opt;
-	swap_a(pile_a, 0);
-	swap_a(pile_b, 0);
+	sw_swap_data(getn_from_start(*pile_a, 0), getn_from_end(*pile_a, 0));
+	sw_swap_data(getn_from_start(*pile_b, 0), getn_from_end(*pile_b, 0));
 }
