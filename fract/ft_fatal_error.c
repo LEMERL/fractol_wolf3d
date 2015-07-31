2a8952c16d3ef838571306587c38b94c63c6a4f3
mgrimald <mgrimald@student.42.fr>
2015-07-31 21:24:42 +0200

    multiple entree (fork) possibles, un leaks restants: impossible a supprimer

--- fract/ft_fatal_error.c
+++ fract/ft_fatal_error.c
@@ -6,7 +6,7 @@
 /*   By: mgrimald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/18 14:49:40 by mgrimald          #+#    #+#             */
-/*   Updated: 2015/07/31 17:00:20 by mgrimald         ###   ########.fr       */
+/*   Updated: 2015/07/31 21:21:45 by mgrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 
 void	ft_fatal_error(t_env *e)
 {
-	(void)e;
-	exit (0);
+	void	*antileaks;
+
+	antileaks = NULL;
+	if (e != NULL)
+	{
+		if (e->img)
+		{
+			if (e->img->ptr)
+				mlx_destroy_image(e->mlx, e->img->ptr);
+			ft_memdel((void**)&(e->img));
+		}
+		if (e->win)
+		{
+			if (e->win->ptr)
+				mlx_destroy_window(e->mlx, e->win->ptr);
+			ft_memdel((void**)&(e->win));
+		}
+		if (e->mlx)
+			antileaks = e->mlx;
+		ft_memdel((void**)&e);
+	}
+	wait(NULL);
+	exit(0);
 }
--- fract/main.c
+++ fract/main.c
@@ -6,13 +6,14 @@
 /*   By: mgrimald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/08 06:05:15 by mgrimald          #+#    #+#             */
-/*   Updated: 2015/06/09 15:39:24 by mgrimald         ###   ########.fr       */
+/*   Updated: 2015/07/31 20:27:03 by mgrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract.h"
+#include <signal.h>
 
-int		main(int argc, char **argv)
+void	start(int argc, char **argv)
 {
 	t_env	*e;
 
@@ -25,11 +26,46 @@ int		main(int argc, char **argv)
 		ft_fatal_error(e);
 	e->win->width = 1000;
 	e->win->height = 1000;
-	e->win->ptr = mlx_new_window(e->mlx, e->win->width, e->win->height, "frct");
+	e->win->ptr = mlx_new_window(e->mlx, e->win->width,
+			e->win->height, argv[argc - 1]);
 	if (e->win->ptr == NULL)
 		ft_fatal_error(e);
 	frct_init(e, e->slc);
 	frct_draw(e);
 	frct_mlx(e);
+}
+
+void	sign_hand(int signnum)
+{
+	(void)signnum;
+	wait4(0, 0, WNOHANG, 0);
+}
+
+void	forking(int argc, char **argv)
+{
+	pid_t	pid;
+
+	signal(SIGCHLD, sign_hand);
+	pid = 1;
+	if (argc > 2 && (pid = fork()) < 0)
+		exit(0);
+	if (pid == 0)
+	{
+		forking(argc - 1, argv);
+		ft_fatal_error(0);
+	}
+	else
+	{
+		start(argc, argv);
+		ft_fatal_error(0);
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc <= 2)
+		start(argc, argv);
+	else
+		forking(argc, argv);
 	return (0);
 }
